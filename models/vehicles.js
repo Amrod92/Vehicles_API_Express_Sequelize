b3f9683714f8355bb8e3a7ea975a2829b292702e
Amrod92 <manlio92--@live.it>
2021-09-21 12:42:21 +0100

    Vehicle model

---
+++ models/vehicles.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Vehicles = sequelize.define('vehicles', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  });
+
+  return Vehicles;
+};
